702c25a006f38d8d0a1763790c9cb6218d87008c
galcohavy10 <96891588+galcohavy10@users.noreply.github.com>
2023-04-03 15:48:51 -0700

    Create Register.js

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { TextField, Button } from '@material-ui/core';
+
+function Register() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Call API to register user
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <TextField
+        label="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Register
+      </Button>
+    </form>
+  );
+}
+
+export default Register;
+
+ 
