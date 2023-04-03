eb522bc0c111039cbf466c094dfb01ed94302a40
galcohavy10 <96891588+galcohavy10@users.noreply.github.com>
2023-04-03 15:47:50 -0700

    Create Login.js

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { TextField, Button } from '@material-ui/core';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Call API to authenticate user
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Login
+      </Button>
+    </form>
+  );
+}
+
+export default Login;
