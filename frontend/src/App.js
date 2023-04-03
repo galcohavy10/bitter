6aac24783859965391859c54cc23076fec10cacf
galcohavy10 <96891588+galcohavy10@users.noreply.github.com>
2023-04-03 15:44:08 -0700

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile/:username" component={Profile} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
