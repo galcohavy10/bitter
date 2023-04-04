e07bbc23ec467bd7ddbb43ff9efb80994289e101
galcohavy10 <96891588+galcohavy10@users.noreply.github.com>
2023-04-04 14:47:39 -0700

    Create tweets.js

---
+++ backend/routes/tweets.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// Import your tweet controller functions here
+// e.g. const { getAllTweets, createTweet } = require('../controllers/tweets');
+
+// Set up your tweet routes
+// e.g. router.get('/', getAllTweets);
+// e.g. router.post('/', createTweet);
+
+module.exports = router;
