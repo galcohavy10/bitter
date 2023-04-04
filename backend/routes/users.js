f8747de662bf556dc56c7706f3240cf8bb86d6f8
galcohavy10 <96891588+galcohavy10@users.noreply.github.com>
2023-04-04 14:47:57 -0700

    Create users.js

---
+++ backend/routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// Import your user controller functions here
+// e.g. const { getUserProfile, followUser } = require('../controllers/users');
+
+// Set up your user routes
+// e.g. router.get('/:username', getUserProfile);
+// e.g. router.post('/:username/follow', followUser);
+
+module.exports = router;
